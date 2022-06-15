eac2379195be425a3160bf518a9878975546fd13
lihan <lihan@wyiot.com>
2022-06-15 15:03:07 +0800

    fix code

--- src/main/java/com/zjsru/gitdemo01/Hello.java
+++ src/main/java/com/zjsru/gitdemo01/Hello.java
@@ -1,7 +1,13 @@
 package com.zjsru.gitdemo01;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
     }
 }
